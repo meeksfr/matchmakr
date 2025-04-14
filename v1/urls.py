72d08effed916b0e3a5b9152539eb71b1bdee9d8
mika sustar <79876888+meeksfr@users.noreply.github.com>
2025-04-14 16:21:57 -0400

    docker working (#7)

--- v1/urls.py
+++ v1/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import (
     SkillViewSet, UserProfileViewSet, CompanyViewSet,
     JobPostingViewSet, ApplicationViewSet, MatchViewSet
@@ -15,4 +16,5 @@ router.register(r'matches', MatchViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', obtain_auth_token, name='api_token_auth'),
 ] 
